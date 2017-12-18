9380b1bd842936dbf79cb2e7c46607f7515561ba
Rick <Zarimax@users.noreply.github.com>
2017-12-18 17:05:11 -0500

    Initial Revision - 100% test score

---
+++ codility_CyclicRotation.c
@@ -0,0 +1,23 @@
+// C99 (gcc 6.2.0)
+#include <string.h>
+
+struct Results solution(int A[], int N, int K) {
+    struct Results result;
+
+    int next_pos = 0;
+    int i;
+    int *B = (int *)0;
+    
+    B = calloc(sizeof(int) * N, sizeof(int));
+    memcpy(B, A, sizeof(int) * N);
+
+    for(i = 0; i < N; i++)
+    {
+        next_pos = (i + K) % N;
+        B[next_pos] = A[i];
+    }
+    
+    result.A = B;
+    result.N = N;
+    return result;
+}
