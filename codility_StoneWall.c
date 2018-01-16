1fbc4ab73a0e27f2839a49fa6872cd8932ade147
Rick <Zarimax@users.noreply.github.com>
2018-01-16 15:56:32 -0500

    Initial version - 92% test score
    fails test case "medium1" with "WRONG ANSWER, got 118 expected 117"

---
+++ codility_StoneWall.c
@@ -0,0 +1,42 @@
+// C99 (gcc 6.2.0)
+int solution(int H[], int N) {
+    int stack[N], top=-1, count = 0, i;
+    
+    for (i = 0; i < N; i++)
+    {
+        if (top == -1)
+        {
+            // there are no entries on the wall height stack - we must add one
+            stack[++top] = H[i];    // push to stack
+            count++;                // increment block count
+        }
+        else
+        {
+            if (H[i] > stack[top])
+            {
+                // if the wall height increases, we must add a new block
+                stack[++top] = H[i];    // push to stack
+                count++;                // increment block count
+            }
+            else
+            {
+                // if the wall height descreases, chew through the stack to see
+                // if an identical wall height was already placed
+                while (H[i] < stack[top])
+                {
+                    top--;  // pop from stack
+                }
+
+                // if an identical wall height exits, we do not need to add a new block
+                // if it does not, however, then we do
+                if (H[i] != stack[top])
+                {
+                    stack[++top] = H[i];    // push to stack
+                    count++;                // increment block count
+                }
+            }
+        }
+    }
+    
+    return count;
+}
